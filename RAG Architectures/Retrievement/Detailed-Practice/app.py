f7478e0d7f7eddb68ffd05845f6bc8f9e5e83016
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-06-13 13:37:20 +0200

    adding the retreived function

--- RAG Architectures/Retrievement/Detailed-Practice/app.py
+++ RAG Architectures/Retrievement/Detailed-Practice/app.py
@@ -7,18 +7,31 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 Embendding_Model_Name= "sentence-transformers/all-MiniLM-L6-v2" 
-Chroma_Path="./tesla_db"
+Chroma_Path=r"C:\Users\Lenovo\GenAI-Agentic-Systems\RAG Architectures\Retrievement\Detailed-Practice\tesla_db"
 Top_k=5
-
+tesla_collection="tesla-10k-2019-to-2023"
 embeddings=HuggingFaceEmbeddings(model_name=Embendding_Model_Name)
 
+
 vectore_Store=Chroma(
+    collection_name=tesla_collection,
     persist_directory=Chroma_Path,
-    embedding_function=embeddings
+    embedding_function=Embendding_Model_Name
 )
 
 retreiver=vectore_Store.as_retriever(
     search_type="similarity",
     search_kwargs={"k":Top_k}
 )
+
+def retreive_chunks(user_query,retreiver):
+    docs=retreiver.invoke(user_query)
+    retreived=[]
+    for i,doc in enumerate(docs):
+        retreived.append({
+            "Index":i,
+            "Text":doc.page_content,
+            "Metadata":doc.metadata
+        })
+    return retreived
 
